765aecba732d91d73626cf01cf29a54926658f15
ITorrenteP <isatorrente10@gmail.com>
2025-12-06 18:25:06 -0500

    feature: finished order restriction

--- good-hamburger/src/app/components/Filter.jsx
+++ good-hamburger/src/app/components/Filter.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 export const Filter = ({onFilterChange}) => {
     return (
-        <div className="flex gap-4">
+        <div className="flex gap-4 text-black">
             <button
                 onClick={() => onFilterChange('all')}
-                className={'bg-blue-500 text-white'}
+                className='bg-blue-500 text-white'
             >
                 All
             </button>
             <button
                 onClick={() => onFilterChange('sandwich')}
-                className={'bg-gray-200'}
+                className='bg-gray-200'
             >
                 Sandwiches
             </button>
             <button
                 onClick={() => onFilterChange('extra')}
-                className={'bg-blue-500 text-white'}
+                className='bg-blue-500 text-white'
             >
                 Extras
             </button>
